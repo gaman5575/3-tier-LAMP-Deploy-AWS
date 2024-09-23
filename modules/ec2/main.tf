826dbf48b8d8d5a23a019369e1599b2049b1af34
gaman5575 <gaman5575@gmail.com>
2024-09-23 13:26:07 +0530

    subnet and ec2 modules

---
+++ modules/ec2/main.tf
@@ -0,0 +1,16 @@
+
+
+# Module template for private application servers
+
+resource "aws_instance" "web_servers" {
+  ami = "ami-0a5c3558529277641"
+  instance_type = "t2.micro"
+  key_name = "key_pair"
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  user_data = "${file("${path.module}/service.sh")}"
+
+  tags = {
+    Name = "var.Name"
+  }
+}
